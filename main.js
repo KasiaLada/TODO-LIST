e9e66334a05a4abc75960514cf4078197e4905ba
Katarzyna Łada <96846727+KasiaLada@users.noreply.github.com>
2024-01-11 11:19:24 +0100

    task done

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ const prepareDOMEvents = () => {
 	ulList.addEventListener('click', checkClick);
 	popupCloseBtn.addEventListener('click', closePopup);
 	popupAddBtn.addEventListener('click', changeTodoText);
+	todoInput.addEventListener('keyup', enterKeyCheck);
 };
 
 const addNewTodo = () => {
@@ -77,7 +78,7 @@ const checkClick = (e) => {
 	} else if (e.target.matches('.edit')) {
 		editTodo(e);
 	} else if (e.target.matches('.delete')) {
-		console.log('delete');
+		deleteTodo(e);
 	}
 };
 
@@ -90,14 +91,33 @@ const editTodo = (e) => {
 
 const closePopup = () => {
 	popup.style.display = 'none';
+	popupInfo.textContent = '';
 };
 
 const changeTodoText = () => {
+	// e.preventDefault();
 	if (popupInput.value !== '') {
 		todoToEdit.firstChild.textContent = popupInput.value;
 		popup.style.display = 'none';
+		popupInfo.textContent = '';
 	} else {
 		popupInfo.textContent = 'Musisz podać jakąś treść!';
+	}
+};
+
+const deleteTodo = (e) => {
+	e.target.closest('li').remove();
+
+	const allTodos = ulList.querySelector('li');
+	if (allTodos.length === 0) {
+		errorInfo.textContent = 'Brak zadań na liście.';
+	}
+};
+
+const enterKeyCheck = (e) => {
+	if (e.key === 'Enter') {
+		addNewTodo()
+		
 	}
 };
 document.addEventListener('DOMContentLoaded', main);
